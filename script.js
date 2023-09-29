a4c52ea8b32ca717b19454063e331b61489b2dec
rudininh <sorows22@gmail.com>
2023-09-29 23:31:03 +0800

    cara membuat json melalui php dan js

---
+++ script.js
@@ -0,0 +1,22 @@
+// let = mahasiswa = {
+//     nama : "Rudini Nor Habibi",
+//     NRP : "19310630",
+//     email : "rudini@gmail.com"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if(xhr.readyState == 4 && xhr.status == 200){
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa)
+//     }
+// }
+
+// xhr.open('GET', 'coba.json', true);
+// xhr.send();
+
+$.getJSON('coba.json', function (data) {
+        console.log(data);
+    });
