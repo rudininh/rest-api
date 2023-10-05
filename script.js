9cd455d02b6b766e68345a314987b26f57488cbf
rudininh <sorows22@gmail.com>
2023-10-05 11:29:14 +0800

    active tidak bisa

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-// let = mahasiswa = {
-//     nama : "Rudini Nor Habibi",
-//     NRP : "19310630",
-//     email : "rudini@gmail.com"
-// }
-
-// console.log(JSON.stringify(mahasiswa));
-
-// let xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function() {
-//     if(xhr.readyState == 4 && xhr.status == 200){
-//         let mahasiswa = JSON.parse(this.responseText);
-//         console.log(mahasiswa)
-//     }
-// }
-
-// xhr.open('GET', 'coba.json', true);
-// xhr.send();
-
-$.getJSON('pizza.json', function (data) {
-        let menu = data.menu_pizza_hut.;
-       
-        $.each(menu, function(i, data) {
-            $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.pasta.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.pasta.nama +'</h5><p class="card-text">'+ data.pasta.deskripsi +'</p><h5>Rp. '+ data.pasta.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
-        });
-
-         
-        $.each(menu, function(i, data) {
-            $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.pasta.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.pasta.nama +'</h5><p class="card-text">'+ data.pasta.deskripsi +'</p><h5>Rp. '+ data.pasta.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
-        });
-
-         
-        $.each(menu, function(i, data) {
-            $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.minuman.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.minuman.nama +'</h5><p class="card-text">'+ data.minuman.deskripsi +'</p><h5>Rp. '+ data.minuman.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
-        });
-        
-    });
+$('.nav-item').on('click', function () {
+    $('.nav-item').removeClass('active');
+    $(this).addClass('active');
+});
--- tes.js
+++ tes.js
@@ -1,27 +1,39 @@
 
 $.getJSON('pizza.json', function (data) {
     let menupizzas = data.menu_pizza_hut.pizzas;
-    let menupasta = data.menu_pizza_hut.pasta;
-    let menuminuman = data.menu_pizza_hut.minuman;
+    // let menupasta = data.menu_pizza_hut.pasta;
+    // let menuminuman = data.menu_pizza_hut.minuman;
 
-   
+    $('#daftar-menu').empty();
     $.each(menupizzas, function(i, data) {
-        $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5>Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+        $('#daftar-menu').append(`
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+            <img src="${data.gambar}" class="card-img-top">
+                <div class="card-body">
+                <h5 class="card-title">${data.nama} </h5>
+                <p class="card-text">${data.deskripsi}</p>
+                <h5>Rp. ${data.harga}</h5>
+                <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                </div>
+            </div>
+        </div>`);
     });
 
-    $.each(menupasta, function(i, data) {
-        $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5>Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
-    });
+    // $.each(menupasta, function(i, data) {
+    //     $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5>Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+    // });
 
      
    
-    $.each(menuminuman, function(i, data) {
-        $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5>Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
-    });
-
-     
-   
-     
-   
+    // $.each(menuminuman, function(i, data) {
+    //     $('#daftar-menu').append('<div class="col-md-4"><div class="card" style="width: 18rem;"><img src="'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5>Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+    // });
+  
     
+});
+
+$('.nav-link').on('click', function () {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
 });
