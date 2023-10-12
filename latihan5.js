716df6a475bfeeb29903af6b15e9a6035a3a39cc
rudininh <sorows22@gmail.com>
2023-10-12 16:18:02 +0800

    REST API #5 APA ITU PUBLIC API ? DONE

--- latihan5.js
+++ latihan5.js
@@ -26,7 +26,7 @@ function SearchMovie(){
                             <div class="card-body">
                             <h5 class="card-title">${data.Title} </h5>
                             <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
-                            <a href="https://www.imdb.com/title/${data.imdbID}/" class="card-link">See Detail</a>
+                            <a href="" class="card-link btn btn-primary see-detail"  data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
                             </div>
                         </div>
                     </div>
@@ -70,10 +70,54 @@ $(document).ready(function() {
 });
 });
 
-// $('#search-input').on('keyup', function(event){
-//     if (event.keyCode === 13){
-//         SearchMovie();
-//     };
+    
+$(document).ready(function() {
+    $('#movie-list').on('click','.see-detail', function() {
+    
+      //  console.log($(this).data('id'));
+
+      $.ajax({
+        
+        url: 'http://www.omdbapi.com/',
+        type: 'GET',
+        dataType: 'JSON',
+        data : {
+            'apikey' : '55563ee7',
+            'i' : $(this).data('id')
+        },
+
+        success : function (movie) {
+            if (movie.Response === "True"){
+
+                $('.modal-body').html(`
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="${movie.Poster}" class="img-fluid">
+                        </div>
+
+                        <div class="col-md-8">
+                            <ul class="list-group">
+                                <li class="list-group-item"><h3>${movie.Title}</h3></li>
+                                <li class="list-group-item">Released : ${movie.Released}</li>
+                                <li class="list-group-item">Genre : ${movie.Genre}</li>
+                                <li class="list-group-item">Director : ${movie.Director}</li>
+                                <li class="list-group-item">Actors : ${movie.Actors}</li>
+                              
+                            </ul>
+                        </div>
+                    </div>    
+                </div>
+                
+                `);
+            };
+        
+        }
 
 
-// });
+      });
+
+
+});
+});
+
