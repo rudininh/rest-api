17765a1e06e1273e88d48a0638b400e55624a93c
rudininh <sorows22@gmail.com>
2024-01-16 16:25:44 +0800

    besoka aja lagi

--- porto/index.php
+++ porto/index.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>My Porfolio</title>
+    <link rel="stylesheet" href="css/style.css" >
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -60,7 +60,7 @@
             <h2>Social Media</h2>
           </div>
         </div>
- 
+        <!-- AIzaSyCCdNym578KnWIiwjePYXgraQR3cHSxPLQ -->
         <div class="row justify-content-center">
           <div class="col-md-5">
             <div class="row">
